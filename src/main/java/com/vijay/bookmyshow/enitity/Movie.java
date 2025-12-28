61de8646892290df6888136ca297af6b628dcec6
Vijayakumar-Developer <vijai1993@gmail.com>
2025-12-28 11:16:34 +0530

    movie controller entity issue fixed

--- src/main/java/com/vijay/bookmyshow/enitity/Movie.java
+++ src/main/java/com/vijay/bookmyshow/enitity/Movie.java
@@ -18,4 +18,33 @@ public class Movie {
 	private String language;
 	private String genre;
 	private Integer duration;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public Integer getDuration() {
+		return duration;
+	}
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
 }
